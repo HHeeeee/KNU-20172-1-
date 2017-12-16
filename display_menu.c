ce688085235575c23c649655f80eaedbd3c79f45
gaki2745 <guyeongjun@naver.com>
2017-12-16 21:19:26 +0900

    메뉴옵션 통합시킴

--- display_menu.c
+++ display_menu.c
@@ -1,8 +1,5 @@
 #include "pong.h"
 
-void tty_mode(int);
-void set_noecho();
-
 int display_menu(void)
 {
 //	int input;
@@ -44,24 +41,69 @@ int display_menu(void)
 		}
 	endwin();
 }
+int option_menu(void)
+{
+	int input;
+	char menu;
+	system("clear");
+	tty_mode(0);
+	set_noecho();
+	
+		printf("\n\t\t");
+		printf("\n\n\t\t\t	O P T I O N	\n");
+		printf("\n\n\t");
+		printf("\n\t\t\t   	   조 작 법 \t\n");
+		printf("\n\t\t\t      | 1P |     | 2p |	\n");
+		printf("\n\t\t\t UP   | A  |     | K  | \n");
+		printf("\n\t\t\t DOWN | Z  |     | M  | \n");
+		printf("\n\t\t");
+		printf("\n\t");
+		printf("\n\n\n\t\t\t\t난이도설정\t");
+		printf("\n\n\n\t\t\t\tEASY   : 1 \t");
+		printf("\n\n\n\t\t\t\tNORMAL : 2 \t");
+		printf("\n\n\n\t\t\t\tHARD   : 3 \t");
+		printf("\n\n\n\n\n\t\t");
+		while(1)
+		{
+			switch(getchar()){
+				case '1':
+					tty_mode(1);
+					system("clear");
+					return 1;
+				case '2':
+					tty_mode(1);
+					system("clear");
+					return 2;
+				case '3':
+					tty_mode(1);
+					system("clear");
+					return 3;
+				}
+		}
+		endwin();
+}
+
 void set_noecho()
 {
 	struct termios ttystate;
-	
+
 	tcgetattr(0,&ttystate);
 	ttystate.c_lflag &= ~ICANON;
-	ttystate.c_lflag &= ~ECHO;
+	ttystate.c_lflag &= ~ ECHO;
 	ttystate.c_cc[VMIN] = 1;
 	tcsetattr(0,TCSANOW,&ttystate);
 }
 
-void tty_mode(int how){
+void tty_mode(int how)
+{
 	static struct termios original_mode;
 
-	if(how == 0){
-		tcgetattr(0, &original_mode);
-	}
+	if(how==0)
+		tcgetattr(0,&original_mode);
 	else
-		tcsetattr(0, TCSANOW, &original_mode);
-
+		tcsetattr(0,TCSANOW,&original_mode);
 }
+
+
+
+
